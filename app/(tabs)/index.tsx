a0a96832adb6498afc2b46a55c66061d7208f38c
Elisee-des <esabidani@gmail.com>
2024-01-24 02:06:35 +0000

    Fin de la bar de navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,19 @@
 import { View, Text } from "react-native";
 import React from "react";
-import { Link } from "expo-router";
+import { Link, Stack } from "expo-router";
+import ExploreHeader from "@/components/ExploreHeader";
+import Listings from "@/components/Listings";
 
 const Page = () => {
   return (
-    <View>
-      <Link href={"/(modals)/login"}>Login</Link>
-      <Link href={"/(modals)/booking"}>Book</Link>
-      <Link href={"/listing/2233"}>Listing detail</Link>
+    <View style={{ flex: 1 }}>
+      <Stack.Screen
+        options={{
+          header: () => <ExploreHeader />,
+        }}
+      />
+
+      <Listings />
     </View>
   );
 };
