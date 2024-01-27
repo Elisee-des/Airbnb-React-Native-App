a2affcf2f728c3f3c68d194eb096c4ce917e22c9
Elisee-des <esabidani@gmail.com>
2024-01-27 01:56:35 +0000

    Debut gestion de la position geographique

---
+++ components/ListingsMap.tsx
@@ -0,0 +1,76 @@
+import { View, Text } from "react-native";
+import React from "react";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import { StyleSheet } from "react-native";
+
+interface Props {
+  listings: any;
+}
+
+const INITIAL_REGION = {
+  latitude: 37.33,
+  longitude: -122,
+  latitudeDelta: 9,
+  longitudeDelta: 9,
+};
+
+const ListingsMap = ({ listings }: Props) => {
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation
+        showsMyLocationButton
+        initialRegion={INITIAL_REGION}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+  marker: {
+    padding: 8,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    elevation: 5,
+    borderRadius: 12,
+    shadowColor: "#000",
+    shadowOpacity: 0.1,
+    shadowRadius: 6,
+    shadowOffset: {
+      width: 1,
+      height: 10,
+    },
+  },
+  markerText: {
+    fontSize: 14,
+    fontFamily: "mon-sb",
+  },
+  locateBtn: {
+    position: "absolute",
+    top: 70,
+    right: 20,
+    backgroundColor: "#fff",
+    padding: 10,
+    borderRadius: 10,
+    elevation: 2,
+    shadowColor: "#000",
+    shadowOpacity: 0.1,
+    shadowRadius: 6,
+    shadowOffset: {
+      width: 1,
+      height: 10,
+    },
+  },
+});
+
+export default ListingsMap;
